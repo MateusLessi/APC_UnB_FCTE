248664df16e4940f9d39123ef8af28aa1aabc929
Lucas Boaventura <lucasxboaventura@hotmail.com>
2025-04-03 21:28:04 -0300

    adicionando codigos das primeiras 2 aulas

---
+++ codigos/aula1/ola_mundo.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    // escrever o algoritmo a partir daqui...
+    printf("Ola Mundo!\n");
+    return 0;
+}
---
+++ codigos/aula2/aula2_1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    char c1;
+    int i;
+
+    scanf("%c %d", &c1, &i);
+    // scanf("%c", &c1);
+    // scanf("%d", &i);
+
+    printf("O inteiro lido eh: %d\n", i);
+    printf("O char lido eh: %c\n", c1);
+
+    return 0;
+}
